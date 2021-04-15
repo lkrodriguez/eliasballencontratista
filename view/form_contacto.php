a7f7e9f0b8a38569528081a78006d42bd808319b
lkrodriguez <41454865+lkrodriguez@users.noreply.github.com>
2021-04-14 21:39:50 -0500

    view-formulario contacto
    se creo la vista de formulario contacto procesos

--- view/form_contacto.php
+++ view/form_contacto.php
@@ -1,6 +1,12 @@
 <?php
 include_once('./all/header.php');
 /*llama el archivo header el cual contiene los estilos booostrap*/
+$nombres =isset($_POST['nombres']) ? $_POST['nombres'] : '';
+$email =isset($_POST['email']) ? $_POST['email'] : '';
+$asunto =isset($_POST['asunto']) ? $_POST['asunto'] : '';
+$mensaje =isset($_POST['mensaje']) ? $_POST['mensaje'] : '';
+
+
 ?>
 
 <BR>
